b2e06ed3c2f907c7d4a364af3d007413e0fa0a51
Marcos Juvencio <marcoslopes.dev@gmail.com>
2025-02-10 19:56:59 -0300

    feat: update Time component to include ticket number input

--- src/components/forms/baseTicket/Time.tsx
+++ src/components/forms/baseTicket/Time.tsx
@@ -6,104 +6,27 @@ import Input from "@/components/ui/input";
 
 export default function Time({ control }: { control: any }) {
   return (
-    <Card
-      style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        gap: 5,
-        marginBottom: 10,
-      }}
-    >
-      <View style={styles.collum2}>
-        <View style={[styles.boxTime]}>
-          <Controller
-            control={control}
-            name="hourInit"
-            render={({ field }) => (
-              <Input
-                label="Hora:"
-                value={field.value}
-                onChangeText={field.onChange}
-                keyboardType="phone-pad"
-                maxLength={2}
-              />
-            )}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <Text style={{ fontSize: 20, margin: 10 }}>:</Text>
-        </View>
-
-        <View style={[styles.boxTime]}>
-          <Controller
-            control={control}
-            name="minuteInit"
-            render={({ field }) => (
-              <Input
-                label="Minuto:"
-                value={field.value}
-                onChangeText={field.onChange}
-                keyboardType="phone-pad"
-                maxLength={2}
-              />
-            )}
-          />
-        </View>
+    <View style={styles.container}>
+      <View>
+        <Controller
+          control={control}
+          name="numberTicket"
+          render={({ field }) => (
+            <Input
+              label="Numero ticket:"
+              value={field.value}
+              onChangeText={field.onChange}
+              keyboardType="phone-pad"
+            />
+          )}
+        />
       </View>
-      <Text> até </Text>
-      <View style={styles.collum2}>
-        <View style={[styles.boxTime]}>
-          <Controller
-            control={control}
-            name="hourFinal"
-            render={({ field }) => (
-              <Input
-                label="Hora:"
-                value={field.value}
-                onChangeText={field.onChange}
-                keyboardType="phone-pad"
-                maxLength={2}
-              />
-            )}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <Text style={{ fontSize: 20, margin: 10 }}>:</Text>
-        </View>
-
-        <View style={[styles.boxTime]}>
-          <Controller
-            control={control}
-            name="minuteFinal"
-            render={({ field }) => (
-              <Input
-                label="Minuto:"
-                value={field.value}
-                onChangeText={field.onChange}
-                keyboardType="phone-pad"
-                maxLength={2}
-              />
-            )}
-          />
-        </View>
-      </View>
-    </Card>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  collum2: {
+  container: {
     flex: 1,
     flexDirection: "row",
   },
