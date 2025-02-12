817876bf55a670e5818d248ee923549a2b941e76
Marcos Juvencio <marcoslopes.dev@gmail.com>
2025-02-12 00:14:18 -0300

    feat: update MenuPrimary component layout and enhance button styles

--- src/components/app/menuPrimary.tsx
+++ src/components/app/menuPrimary.tsx
@@ -1,23 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
-import { Home, List, Plus } from "lucide-react-native";
+import { ClipboardPlus, Home, List } from "lucide-react-native";
 import { Colors } from "@/constants/Colors";
 
 export default function MenuPrimary() {
   return (
     <View style={styles.menu}>
       <Link style={[styles.btn, styles.btnHome]} href="/" replace>
-        <Home color={Colors.colorIconsLight} size={30} />
+        <Home color={Colors.colorIconsLight} size={36} />
+      </Link>
+      <Link style={[styles.btnInit]} href="/action" replace>
+        {/* <Text style={styles.btnInitText}>Iniciar</Text> */}
+        <ClipboardPlus color={Colors.colorIconsLight} size={36} />
       </Link>
       <Link style={[styles.btn]} href="/list" replace>
-        <List color={Colors.colorIconsLight} size={30} />
-      </Link>
-      <Link style={[styles.btn, { flex: 1 }]} href="/action" replace>
-        <View style={styles.btnInit}>
-          {/* <Plus color={Colors.colorIconsLight} size={30} /> */}
-          <Text style={styles.btnInitText}>Iniciar ticket</Text>
-        </View>
+        <List color={Colors.colorIconsLight} size={36} />
       </Link>
     </View>
   );
@@ -26,39 +24,33 @@ export default function MenuPrimary() {
 const styles = StyleSheet.create({
   menu: {
     position: "absolute",
-    bottom: 20,
+    bottom: 0,
     left: 0,
     right: 0,
     flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
     gap: 10,
     backgroundColor: "#333",
     paddingHorizontal: 9,
     paddingVertical: 9,
-    borderRadius: 9999,
-    marginHorizontal: 5,
   },
   btn: {
-    padding: 20,
-    backgroundColor: Colors.btnPrimary,
-    borderRadius: 9999,
-    borderWidth: 2,
-    // borderColor: "#fff",
+    padding: 10,
   },
   btnHome: {
-    backgroundColor: "#353b48",
+    // backgroundColor: "#353b48",
   },
   btnInit: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 30,
-    fontSize: 20,
-    color: "#fff",
+    borderRadius: 9999,
+    // paddingHorizontal: 25,
+    // paddingVertical: 10,
+    padding: 15,
   },
   btnInitText: {
     color: "#fff",
     fontSize: 18,
+    paddingHorizontal: 15,
     textTransform: "uppercase",
   },
 });
